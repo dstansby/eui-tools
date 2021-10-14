4f86b73799cf76406c5a220cbe3ae3739e2e886b
David Stansby <dstansby@gmail.com>
2021-10-14 14:59:26 +0100

    Add some vmin/vmax limits

--- products.py
+++ products.py
@@ -10,9 +10,10 @@ class Product:
 
 
 FSI174 = Product('fsi174', 'solar orbiterfsi174', vmin=1e1, vmax=1e4)
-FSI304 = Product('fsi304', 'solar orbiterfsi304')
-HRIEUV174 = Product('hrieuv174', 'solar orbiterhri_lya1216')
-HRILYA1216 = Product('hrilya1216', 'solar orbiterhri_euv174')
+FSI304 = Product('fsi304', 'solar orbiterfsi304', vmin=1e1, vmax=1e4)
+HRIEUV174 = Product('hrieuv174', 'solar orbiterhri_euv174')
+HRILYA1216 = Product('hrilya1216', 'solar orbiterhri_lya1216',
+                     vmin=1e2, vmax=1e4)
 
 all_products = [FSI174, FSI304, HRIEUV174, HRILYA1216]
 

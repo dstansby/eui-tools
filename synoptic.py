eb16bbd3448a277cd26070c8ac97f906cf7a5bbe
David Stansby <dstansby@gmail.com>
2021-10-18 17:29:58 +0100

    Synoptic maps working

--- synoptic.py
+++ synoptic.py
@@ -1,8 +1,19 @@
-from astropy.time import Time, TimeDelta
 import astropy.units as u
+import matplotlib.colors as mcolor
+import matplotlib.pyplot as plt
+import numpy as np
+import sunpy.sun.constants
+
+from astropy.coordinates import SkyCoord
+from astropy.time import Time, TimeDelta
+from astropy.wcs import WCS
+from reproject import reproject_interp
+from sunpy.map import Map, make_fitswcs_header
 
 from products import *
-from utils import get_closest_map
+from utils import get_closest_map, get_aia_map
+
+SHAPE_OUT = [360, 720]
 
 
 def generate_synoptic_images(product):
@@ -13,10 +24,86 @@ def generate_synoptic_images(product):
     delta = TimeDelta(12 * u.h)
 
     while t < endtime:
-        m = get_closest_map(product, t, delta / 2)
-        if m is None:
+        eui_map = get_closest_map(product, t, delta / 2)
+        if eui_map is None:
             print(f'💔  No EUI data available near {t.iso}')
+            t += delta
             continue
-        print(m)
+
+        aia_map = get_aia_map(product, t, delta)
+        if aia_map is None:
+            print(f'💔💔💔  No AIA data available near {t.iso}')
+            t += delta
+            continue
+
+        eui_reproj = reproject_carrington(eui_map, t)
+        eui_reproj.plot_settings['norm'] = mcolor.LogNorm(vmin=1e1, vmax=1e4)
+        eui_reproj.plot_settings['cmap'] = 'sdoaia171'
+
+        aia_reproj = reproject_carrington(aia_map, t)
+
+        total = np.nanmean(np.stack((eui_reproj.data,
+                                     aia_reproj.data * 2),
+                                    axis=-1),
+                           axis=-1)
+        total_map = Map(total, aia_reproj.wcs)
+        total_map.plot_settings['norm'] = mcolor.LogNorm(vmin=1e1, vmax=1e4)
+        total_map.plot_settings['cmap'] = 'sdoaia171'
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection=total_map)
+        total_map.plot(axes=ax)
+
+        eui_map['date-obs'] = t.isot
+        aia_map['date-obs'] = t.isot
+        limb_eui, _ = eui_map.draw_limb(axes=ax, color='tab:blue')
+        limb_aia, _ = aia_map.draw_limb(axes=ax, color='tab:green')
+        ax.legend([limb_eui, limb_aia], ['EUI limb', 'AIA limb'])
+        fig.savefig(t.strftime('synoptic_%Y%m%d_%H%M%S.png'), dpi=300)
 
         t += delta
+
+
+def synop_header(shape_out, dtime):
+    """
+    Generate a synoptic map header in a Carrington frame of reference.
+
+    Parameters
+    ----------
+    shape_out : [int, int]
+    dtime : astropy.time.Time
+
+    Returns
+    -------
+    dict
+    """
+    frame_out = SkyCoord(180, 0, unit=u.deg,
+                         frame="heliographic_carrington",
+                         obstime=dtime,
+                         observer='earth')
+    header = make_fitswcs_header(shape_out, frame_out,
+                                 scale=[180 / shape_out[0],
+                                        360 / shape_out[1]] * u.deg / u.pix,
+                                 projection_code="CAR")
+    return header
+
+
+def reproject_carrington(m, date):
+    """
+    Reproject a map into Heliographic Carrington coordinates.
+
+    Parameters
+    ----------
+    m : sunpy.map.Map
+
+    Returns
+    -------
+    m : sunpy.map.Map
+    """
+    m.meta['rsun_ref'] = sunpy.sun.constants.radius.to_value(u.m)
+    m.meta['date-obs'] = date.isot
+    header = synop_header(SHAPE_OUT, m.date)
+    wcs = WCS(header)
+    # with np.errstate(invalid='ignore'):
+    array, footprint = reproject_interp(m, wcs, shape_out=SHAPE_OUT)
+    return Map((array, header))
--- utils.py
+++ utils.py
@@ -2,10 +2,20 @@ import functools
 import glob
 from pathlib import Path
 
+from aiapy.calibrate import (update_pointing, fix_observer_location,
+                             correct_degradation, normalize_exposure)
+from aiapy.calibrate.util import get_correction_table
 from astropy.time import Time
+import astropy.units as u
 import numpy as np
+from sunpy.net import Fido, attrs as a
+from sunpy.map import Map
+import sunpy.sun.constants
 
 from config import fits_dir
+from products import *
+
+correction_table = get_correction_table()
 
 
 def remove_duplicates(map_list):
@@ -17,7 +27,7 @@ def remove_duplicates(map_list):
     return names
 
 
-@functools.lru_cache()
+@functools.lru_cache(maxsize=4)
 def get_all_files(product):
     files = glob.glob(str(fits_dir / f'solo_L2_eui-{product.name}-image*.fits'))
     return remove_duplicates(files)
@@ -27,7 +37,7 @@ def get_eui_date(fname, product):
     return Time.strptime(fname[:-12], f'solo_L2_eui-{product.name}-image_%Y%m%dT%H%M%S')
 
 
-@functools.lru_cache()
+@functools.lru_cache(maxsize=4)
 def all_eui_dates(product):
     """
     Get the dates of all available files for a specific product.
@@ -47,8 +57,69 @@ def all_eui_dates(product):
 
 
 def get_closest_map(product, t, max_delta):
+    """
+    Get closest EUI map to a given time.
+
+    Parameters
+    ----------
+    product : Product
+    t : astropy.time.Time
+    max_delta : astropy.time.TimeDelta
+        Max timedelta threshold.
+
+    Returns
+    -------
+    sunpy.map.Map
+    """
     dates = all_eui_dates(product)
     idx = np.argmin(np.abs(dates - t))
     if np.abs(dates[idx] - t) > max_delta:
         return
-    return get_all_files(product)[idx]
+    fname = get_all_files(product)[idx]
+    return Map(fname)
+
+
+def get_aia_map(product, t, max_delta):
+    """
+    Get closest AIA map to a given time that matches an EUI product.
+
+    Parameters
+    ----------
+    product : Product
+    t : astropy.time.Time
+    max_delta : astropy.time.TimeDelta
+        Max timedelta threshold.
+
+    Returns
+    -------
+    sunpy.map.Map
+    """
+    t = a.Time(t - max_delta, t + max_delta, t)
+    if product is FSI174:
+        wlen = a.Wavelength(171 * u.Angstrom)
+    result = Fido.search(t & wlen & a.Instrument('AIA'))
+    if len(result[0]) == 0:
+        return
+
+    fname = Fido.fetch(result)[0]
+    return prep_aia(Map(fname))
+
+
+def prep_aia(m):
+    """
+    Prep an AIA map.
+
+    This runs (in order):
+        - `aiapy.calibrate.update_pointing`
+        - `aiapy.calibrate.fix_observer_location`
+        - `aiapy.caibrate.correct_degradation`
+        - `aiapy.calibrate.normalize_exposure`
+    """
+    print('Prepping map')
+    if m.exposure_time <= 0 * u.s:
+        raise RuntimeError('Exposure time <= 0')
+    m = update_pointing(m)
+    m = fix_observer_location(m)
+    m = correct_degradation(m, correction_table=correction_table)
+    m = normalize_exposure(m)
+    return m
